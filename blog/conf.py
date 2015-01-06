fd992aa98bf512cade7c05bec7bbd31d71172793
Derrick Gilland <dgilland@gmail.com>
2015-01-06 16:25:44 -0500

    Replace leapday theme with sparse theme based on Bootstrap v3.

--- blog/conf.py
+++ blog/conf.py
@@ -119,7 +119,7 @@ NAVIGATION_LINKS = {
 }
 
 # Name of the theme to use.
-THEME = "leapday"
+THEME = "sparse"
 
 # Below this point, everything is optional
 
@@ -487,7 +487,7 @@ LICENSE = ""
 # A small copyright notice for the page footer (in HTML).
 # (translatable)
 # CONTENT_FOOTER = 'Contents &copy; {date}         <a href="mailto:{email}">{author}</a> - Powered by         <a href="http://getnikola.com" rel="nofollow">Nikola</a>         {license}'
-CONTENT_FOOTER = '<a href="mailto:{email}">{author}</a> - Powered by <a href="http://getnikola.com" rel="nofollow">Nikola</a> {license}'
+CONTENT_FOOTER = '<a href="mailto:{email}">{author}</a> - Blog powered by <a href="http://getnikola.com" rel="nofollow">Nikola</a> {license}'
 
 # Things that will be passed to CONTENT_FOOTER.format().  This is done
 # for translatability, as dicts are not formattable.  Nikola will
