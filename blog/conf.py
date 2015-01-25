da7f155f71372335397c720ae16051b88c3faa13
Derrick Gilland <dgilland@gmail.com>
2015-01-25 00:06:11 -0500

    Add github and email links to nav. Convert nav to all icons.

--- blog/conf.py
+++ blog/conf.py
@@ -111,10 +111,12 @@ TRANSLATIONS_PATTERN = "{path}.{lang}.{ext}"
 
 NAVIGATION_LINKS = {
     DEFAULT_LANG: (
-        ("/", '<i class="fa fa-home"></i>'),
-        ("/archive.html", "Archive"),
-        ("/categories/index.html", "Tags"),
-        ("/rss.xml", '<i class="fa fa-rss"></i>'),
+        ('/', '<i class="fa fa-home fa-lg"></i>'),
+        ('/archive.html', '<i class="fa fa-archive fa-lg"></i>'),
+        ('/categories/index.html', '<i class="fa fa-tags fa-lg"></i>'),
+        ('https://github.com/dgilland', '<i class="fa fa-github fa-lg"></i>'),
+        ('mailto:dgilland@gmail.com', '<i class="fa fa-envelope fa-lg"></i>'),
+        ('/rss.xml', '<i class="fa fa-rss fa-lg"></i>'),
     ),
 }
 
